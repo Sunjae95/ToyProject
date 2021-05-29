6bd1e8e7277a45f2ccbfa269a8be63f6afe37e8c
Sunjae95 <tjswo9502@gmail.com>
2021-05-29 21:15:52 +0900

    문제가져오기 함수 구현 parameter정의 X

--- server/routes/problem.js
+++ server/routes/problem.js
@@ -5,7 +5,7 @@ const router = express.Router();
 router.get("/", async (req, res) => {
   try {
     const data = await getProblem();
-    console.log(typeof data);
+    
     res.json(data);
   } catch (e) {
     console.log("getProblem Error:", e);
