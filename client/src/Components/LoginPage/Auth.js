9e913bcffb0de1f802ee127ffde075f920f57b92
Sunjae95 <tjswo9502@gmail.com>
2021-05-30 18:41:48 +0900

    Auth컴포넌트 수정

--- client/src/Components/LoginPage/Auth.js
+++ client/src/Components/LoginPage/Auth.js
@@ -1,19 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Redirect } from 'react-router';
 import { API_ENDPOINT } from 'Utility/config';
 import { requestPOST } from 'Api/index';
 
 function Auth() {
-  const authCode = location.search.slice(6);
-  const bodyData = { authCode };
-  requestPOST(`${API_ENDPOINT}/login/auth`, bodyData)
-    .then(res => res.json())
-    .then(res => {
-      localStorage.setItem('user', res.user);
-    });
-
-  return <Redirect to="/" />;
+  const [sucess, setSucess] = useState(false);
+  useEffect(async () => {
+    try {
+      const authCode = location.search.slice(6);
+      const bodyData = { authCode };
+      const tmp = await requestPOST(`${API_ENDPOINT}/login/auth`, bodyData);
+      const data = await tmp.json();
+      setSucess(true);
+      localStorage.setItem('user', data.user);
+    } catch {
+      console.log('Auth: ', console.log(e));
+    }
+  }, []);
+  if (sucess) {
+    return <Redirect to="/" />;
+  } else {
+    return <div>인증오류!!</div>;
+  }
 }
 
 export default Auth;
-
