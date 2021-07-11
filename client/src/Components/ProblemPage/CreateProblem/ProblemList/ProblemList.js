ededfe5520bda8a5da8eb7b4f1eb092d7b633cf1
Sunjae95 <tjswo9502@gmail.com>
2021-07-12 01:02:50 +0900

    카드 뒤집기 기능 구현

--- client/src/Components/ProblemPage/CreateProblem/ProblemList/ProblemList.js
+++ client/src/Components/ProblemPage/CreateProblem/ProblemList/ProblemList.js
@@ -1,24 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import '../CreationPage.css';
 
 function Problem({ no, grade, title }) {
+  const [card, setCard] = useState(false);
+
+  const onFilp = () => {
+    setCard(!card);
+  };
+
+  if (!card)
+    return (
+      <div className={'problemItem'} onClick={onFilp}>
+        뒷면
+      </div>
+    );
+
   return (
-    <div>
-      문제번호: {no}
-      문제등급: {grade}
-      문제제목: {title}
-    </div>
+    <>
+      <div className="problemItem" onClick={onFilp}>
+        <p>문제제목: {title}</p>
+        <p>문제번호: {no}</p>
+        <p>문제등급: {grade}</p>
+      </div>
+    </>
   );
 }
 
 function ProblemList({ problems }) {
   return (
-    <div
-      style={{
-        height: '300px',
-        boxSizing: 'border-box'
-      }}
-    >
+    <div>
       {problems &&
         [...problems].map((problem, index) => {
           return (
